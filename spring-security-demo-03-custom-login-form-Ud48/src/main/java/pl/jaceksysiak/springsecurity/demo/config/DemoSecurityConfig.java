79ae76cc13780b98f48b0e81afa1b5fd4d0da350
Jacek <Jacek@BERLIN>
2020-03-10 06:38:05 +0100

    cc

--- spring-security-demo-03-custom-login-form-Ud48/src/main/java/pl/jaceksysiak/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form-Ud48/src/main/java/pl/jaceksysiak/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package pl.jaceksysiak.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,7 +24,19 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("bb").password("bb").roles("MANAGER"))
 			.withUser(users.username("cc").password("cc").roles("ADMIN"));
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
+		
+	}
 }
 
 
